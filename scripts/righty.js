1627dce91e2869023d911e2e246493499b39d34e
Mark Szymanski <szyam@hotmail.com>
2015-09-26 21:48:27 -0400

    righty.css - grid and toolbox

---
+++ scripts/righty.js
@@ -0,0 +1,93 @@
+jQuery(document).ready(function($){
+	
+	// * page scrolling http://css-tricks.com/snippets/jquery/smooth-scrolling/
+	$(function() {
+		$('a[href*=#]:not([href=#])').click(function() {
+			if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+				var target = $(this.hash);
+				target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+				if (target.length) {
+					$('html,body').animate({
+						scrollTop: target.offset().top
+						}, 400);
+					return false;
+				}
+			}
+		});
+	});
+	// * Page scrolling
+
+
+	// * Scroll Nav - https://jsfiddle.net/mariusc23/s6mLJ/31/
+	var didScroll;
+	var lastScrollTop = 0;
+	var delta = 5;
+	var navbarHeight = $('.righty-nav').outerHeight();
+
+	$(window).scroll(function(event){
+	    didScroll = true;
+	});
+
+	setInterval(function() {
+	    if (didScroll) {
+	        hasScrolled();
+	        didScroll = false;
+	    }
+	}, 250);
+
+	function hasScrolled() {
+	    var st = $(this).scrollTop();
+	    // * Make sure they scroll more than delta
+	    if(Math.abs(lastScrollTop - st) <= delta)
+	        return;
+	    
+	    // * If they scrolled down and are past the navbar, add class .nav-up.
+	    // * This is necessary so you never see what is "behind" the navbar.
+	    if (st > lastScrollTop && st > navbarHeight){
+	        // Scroll Down
+	        $('.righty-nav').removeClass('nav-down').addClass('nav-up');
+	    } else {
+	        // * Scroll Up
+	        if(st + $(window).height() < $(document).height()) {
+	            $('.righty-nav').removeClass('nav-up').addClass('nav-down');
+	        }
+	    }
+	    lastScrollTop = st;
+	}
+	// * End scroll nav
+
+	// * Menu Toggle
+	// * var menuSize = $('.menu li').size();
+
+	$('#toggle').on('click', function(){
+		$('.menu')
+		.toggleClass('mobile')
+		.find('a')
+		.css( styles );
+	});
+
+	// * End Menu Toggle
+
+	// * Breakpoint Finder
+	function viewportWidth() {
+	    var width = $(window).width();
+	    var widthEm = (1/16*width)
+	    $('#browser-width').text(widthEm + ' em').css('position', 'fixed');
+	}
+
+	$( "body" )
+		.prepend( '<span id="browser-width" style="\
+				background-color:gainsboro;\
+				padding:0.5em;\
+				min-width:125px;\
+				bottom:0;\
+				right:0;\
+				display:block;\
+				z-index:1000">\
+			</span>' );
+	viewportWidth();
+
+	$(window).resize(viewportWidth);
+	// * End Breakpoint Finder
+
+}); // * End On Ready
